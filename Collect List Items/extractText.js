e4bf9f3e483597f4f308b759c86c70d57d919eb1
dannielaAntonio <dannielaanntonio@gmail.com>
2022-06-29 21:54:58 +0300

    added extractText()

--- Collect List Items/extractText.js
+++ Collect List Items/extractText.js
@@ -1,1 +1,9 @@
+function extractText() {
 
+    let items = Array.from(document.querySelectorAll('li'));
+    let result = items.map(e => e.textContent).join('\n');
+    
+    let textArEl = document.getElementById('textArr');
+    textArEl.textContent = result;
+    
+}
