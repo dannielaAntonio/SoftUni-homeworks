09239250081e2ff448645c3708388503a72d8e61
dannielaAntonio <dannielaanntonio@gmail.com>
2022-06-30 00:42:27 +0300

    calculate numbers

--- Sum Numbers/sumNum.js
+++ Sum Numbers/sumNum.js
@@ -1,1 +1,11 @@
+function calculate() {
 
+    let first = document.getElementById('firstInput');
+    let second = document.getElementById('secondInput');
+
+    let result = Number(first.value) + Number(second.value);
+    
+    let calc = document.getElementById('resultInput');
+    calc.value = result;
+
+}
