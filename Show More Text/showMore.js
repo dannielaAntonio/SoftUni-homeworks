da9b4fded43188090a2947603c18451564a4f3cc
dannielaAntonio <dannielaanntonio@gmail.com>
2022-06-30 14:41:27 +0300

    Update showMore.js

--- Show More Text/showMore.js
+++ Show More Text/showMore.js
@@ -1,12 +1,4 @@
 function showText() {
-    let element = document.getElementById('text');
-    let text = element.textContent;
-    // console.log(element);
-    // console.log(text);
-
-    element.style.display = '';
-
-    let readMoreLink = document.getElementById('more');
-    readMoreLink.style.display = 'none';
-
+    document.getElementById('text').style.display = '';
+    document.getElementById('more').style.display = 'none';
 }
